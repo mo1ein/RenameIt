5ac4f7ccbc324bd9c2bd9c8de97f282760d2f543
mo1ein <moeinn.com@gmail.com>
2020-03-11 11:00:48 +0330

    Change setup.py and packaging

--- setup.py
+++ setup.py
@@ -1,19 +1,52 @@
-
-import os 
-
-#install Rit :))
-os.system('sudo apt install ./Rit_1.0.0_all.deb')
+#!/bin/python3 
 
 
-# finding current directory
-cwd = os.path.abspath(__file__)
-setup_dir = os.path.dirname(cwd)
+import os
+from setuptools import setup
 
-# Creating man page file
-Rit_man_page = os.path.join(setup_dir, 'man', 'Rit.1')
-os.system('gzip -f -k -9 "' + Rit_man_page + '"')
-cp = Rit_man_page + '.gz'
-os.system('sudo cp %s /usr/share/man/man1/'%(cp))
 
-print('man page is generated!')
+
+def readme():
+    with open("README.md", 'r') as f:
+        return f.read()
+
+
+
+
+
+setup(
+    name='Rit',
+    version='1.0.0',
+    description='Rit Rename your photos :)',
+    long_description=readme(),
+    url='https://github.com/mo1ein/RenameIt',
+    license='GPLv3+',
+    author="Moein Halvaei",
+    author_email="moeinn.com@gmail.com",
+    packages=['Rit'],
+    include_package_data= True ,
+    classifiers=[
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
+    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Programming Language :: Python :: Implementation :: PyPy',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Topic :: Software Development :: Localization',
+    'Topic :: Utilities',
+    'Private :: Do Not Upload',
+    ],
+)
 
